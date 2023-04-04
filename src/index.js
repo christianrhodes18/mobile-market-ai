d66196a80a285c98e4d864e7c02b3e28bfec9b49
christianrhodes18 <thechristianrhodes@gmail.com>
2023-04-04 17:12:46 -0400

    new view for desktop

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ const timeline = gsap.timeline({defaults: {ease: "power2.out" ,duration: 1}});
 timeline
     .from('.title-section', {y: '-100%', delay: 0.5})
     .from('.grid-item', {opacity: 0, y: 30, stagger: 0.2}, 1)
-    .from('footer', {y: '150%', ease: 'elastic', duration: 1.5}, 1.5)
+    .from('footer', {y: '150%', ease: 'elastic', duration: 1.5, delay: 0});
+
+// hover animation
+gsap.utils.toArray(".grid-item").forEach(elem => {
+    let hover = gsap.to(elem, {scale: 1.05, duration: .3, paused: true, ease: "power1.inOut", cursor: "pointer"});
+    elem.addEventListener("mouseenter", () => hover.play());
+    elem.addEventListener("mouseleave", () => hover.reverse());
+});
