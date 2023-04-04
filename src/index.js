d3beadcf3784cf4823e8891fa9d505b67bdd7cfc
christianrhodes18 <thechristianrhodes@gmail.com>
2023-04-04 14:21:47 -0400

    add gsap animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("test")
+// gsap animations
+const timeline = gsap.timeline({defaults: {ease: "power2.out" ,duration: 1}});
+timeline
+    .from('.title-section', {y: '-100%', delay: 0.5})
+    .from('.grid-item', {opacity: 0, y: 30, stagger: 0.2}, 1)
+    .from('footer', {y: '150%', ease: 'elastic', duration: 1.5}, 1.5)
